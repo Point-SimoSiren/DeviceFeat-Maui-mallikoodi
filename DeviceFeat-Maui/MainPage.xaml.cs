d021ab229940f3c8c6e117ef13ddef856db2595a
Simppa <simo.siren@careeria.fi>
2025-03-05 12:47:47 +0200

    Muistio tehty

--- DeviceFeat-Maui/MainPage.xaml.cs
+++ DeviceFeat-Maui/MainPage.xaml.cs
@@ -3,11 +3,84 @@
     public partial class MainPage : TabbedPage
     {
 
+        // Muistion tallennuspaikan alustaminen muuttujaksi
+        string fileName = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder
+            .LocalApplicationData), "muistio.txt");
 
+        string text = "";
+
+
+        // Konstruktorimetodi joka suoritetaan kun sivu latautuu
         public MainPage()
         {
+            // Ladataan xaml
             InitializeComponent();
+
+            // Asetusarvot
+
+            // Sliderin arvoasteikko
+            vahvistusKytkin.Minimum = 0;
+            vahvistusKytkin.Maximum = 100;
+
+
+            // Onko tiedostoa ennestään olemassa
+            bool doesExist = File.Exists(fileName);
+
+            if (doesExist == true)
+            {
+                text = File.ReadAllText(fileName);
+                if (text.Length > 0)
+                {
+                    outputLabel.Text = text;
+                }
+                else
+                {
+                    outputLabel.Text = "Mitään ei ole talletettu muistioon.";
+                }
+
+            }
+            else
+            {
+                outputLabel.Text = "Tervetuloa uudelle käyttäjälle!";
+            }
+
         }
+
+        // -----x-----x----- MUISTION TOIMINNOT ----x-------x-------x---------
+
+        // Muistiinpanon tallentaminen
+        private void tallennusNappi_Clicked(object sender, EventArgs e)
+        {
+            text = text + Environment.NewLine + inputKentta.Text;
+            File.WriteAllText(fileName, text);
+            outputLabel.Text = text;
+            inputKentta.Text = "";
+        }
+
+        private void poistoNappi_Clicked(object sender, EventArgs e)
+        {
+            poistoNappi.IsVisible = false;
+            vahvistusInfo.IsVisible = true;
+            vahvistusKytkin.IsVisible = true;
+        }
+
+        private void vahvistusKytkin_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            if (vahvistusKytkin.Value > 90)
+            {
+                //Vibration.Vibrate();
+                vahvistusKytkin.Value = 0;
+                text = "";
+                outputLabel.Text = "";
+                vahvistusKytkin.IsVisible = false;
+                vahvistusInfo.IsVisible = false;
+                poistoNappi.IsVisible = true;
+            }
+        }
+
+
+        // -----x-----x----- LAMPPU ----x-------x-------x---------
+
 
         private async void valo_nappi_on_Clicked(object sender, EventArgs e)
         {
